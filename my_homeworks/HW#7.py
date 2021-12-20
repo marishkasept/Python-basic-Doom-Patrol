734d13ac111ebdedd487315bbf202a038c721d45
Мария <martynovalala@gmail.com>
2021-12-20 15:48:42 +0200

    everything is done

--- my_homeworks/HW#7.py
+++ my_homeworks/HW#7.py
@@ -1,57 +1,3 @@
-# # **Garden**
-# We have only one garden. In the garden can have vegetables, fruits and pests.
-# Each fruit or vegetable should have a stage of maturity (stages: None, Flowering, Green, Red).
-# Pests can eat only green and red fruit or vegetable. Once pests eat the fruit, it should be removed.
-
-# ## There is a Tomato with the following characteristics:
-# 1. Number of tomatoes (Index)
-# 2. Vegetable type
-
-# ### A tomato can:
-# 1. Grow (move to the next stage of maturation)
-# 2. Provide information about your maturity
-
-# ### There is a Tomato Bush that:
-# 1. Contains a list of tomatoes that grow on it
-
-# ### And:
-# 1. Grow with tomatoes
-# 2. Provide information on the maturity of all tomatoes
-# 3. Provide harvest
-
-# ## There is an Apple with the following characteristics:
-# 1. Number of apples (Index)
-# 2. Fruit type
-
-# ### An apple can:
-# 1. Grow (move to the next stage of maturation)
-# 2. Provide information about your maturity
-
-# ### There is an Apple Tree that:
-# 1. Contains a list of apples that grow on it
-
-# ### And:
-# 1. Grow with apples
-# 2. Provide information on the maturity of all apples
-# 3. Provide harvest
-
-# ## And there is also a Gardener who has:
-# 1. Name
-# 2. The plants he looks after
-
-# ### And:
-# 1. Take care of the plant
-# 2. Poison the pests
-# 2. Harvest from it
-
-# ## And there is also a Pests who have:
-# 1. Type
-# 2. Quantity
-
-
-# ## And:
-# 1. Eat the plants
-
 from abc import ABC, abstractmethod
 
 stages = {0: 'None', 1: 'Flowering', 2: 'Green', 3: 'Red'}
@@ -75,8 +21,7 @@ class Garden(metaclass=GardenMeta):
         self.pests = pests
 
     def see_the_garden(self):
-        print(f"There are {self.vegetables} vegetables, {self.fruits} and {self.pests} pests")
-        print("\n")
+        print(f"There are {self.vegetables.number_of_fruits} {self.vegetables.all_fruits[0].veg_type if self.vegetables.number_of_fruits > 0 else ''} tomato vegetables, {self.fruits.number_of_fruits} {self.fruits.all_fruits[0].fruit_type if self.fruits.number_of_fruits > 0 else ''}apple fruits and {', '.join(pest.pest_type for pest in self.pests)} pests in the garden")
 
 
 class Vegetable(ABC):
@@ -110,10 +55,11 @@ class Fruit(ABC):
 
 
 class Tomato(Vegetable):
+    name = 'tomato'
 
-    def __init__(self, tomato_index, tomato_type):
-        self.tomato_index = tomato_index
-        self.tomato_type = tomato_type
+    def __init__(self, tomato_index, veg_type):
+        self.index = tomato_index
+        self.veg_type = veg_type
         self.stage = 0
 
     def grow(self):
@@ -124,28 +70,32 @@ class Tomato(Vegetable):
         return self.stage == 3
 
     def grow_info(self):
-        print(f"{self.tomato_type} - {self.tomato_index}: {stages[self.stage]}")
+        print(f"{self.veg_type} tomato - {self.index}: {stages[self.stage]}")
 
 class TomatoBush:
 
-    def __init__(self, number_of_tomatoes):
-        self.all_tomatoes = [Tomato('Rose', index) for index in range(number_of_tomatoes)]
+    def __init__(self, number_of_fruits):
+        self.number_of_fruits = number_of_fruits
+        self.all_fruits = [Tomato(index, 'Rose') for index in range(number_of_fruits)]
 
     def grow_all(self):
-        for tomato in self.all_tomatoes:
+        for tomato in self.all_fruits:
             tomato.grow()
             tomato.grow_info()
 
     def is_all_ripe(self):
-        return all([tomato.is_ripe() for tomato in self.all_tomatoes])
+        return all([tomato.is_ripe() for tomato in self.all_fruits])
 
     def give_harvest(self):
-         self.all_tomatoes = []
+        self.all_fruits = []
+        self.number_of_fruits = 0
 
 class Apple(Fruit):
-    def __init__(self, apple_index, apple_type):
-        self.apple_index = apple_index
-        self.apple_type = apple_type
+    name = 'apple'
+
+    def __init__(self, apple_index, fruit_type):
+        self.index = apple_index
+        self.fruit_type = fruit_type
         self.stage = 0
 
     def grow(self):
@@ -156,22 +106,78 @@ class Apple(Fruit):
         return self.stage == 3
 
     def grow_info(self):
-        print(f"{self.apple_type} - {self.apple_index}: {stages[self.stage]}")
+        print(f"{self.fruit_type} apple - {self.index}: {stages[self.stage]}")
 
 class AppleTree:
-    def __init__(self, number_of_apples):
-        self.all_apples = [Apple('Golden', index) for index in range(number_of_apples)]
+    def __init__(self, number_of_fruits):
+        self.number_of_fruits = number_of_fruits
+        self.all_fruits = [Apple(index, 'Golden') for index in range(number_of_fruits)]
 
     def grow_all(self):
-        for apple in self.all_apples:
+        for apple in self.all_fruits:
             apple.grow()
             apple.grow_info()
 
     def is_all_ripe(self):
-        return all([apple.is_ripe() for apple in self.all_apples])
+        return all([apple.is_ripe() for apple in self.all_fruits])
 
     def give_harvest(self):
-        self.all_apples = []
+        self.all_fruits = []
+        self.number_of_fruits = 0
+
+
+class Gardener:
+    def __init__(self, name, plants_to_care):
+        self.name = name
+        self.plants_to_care = plants_to_care
+
+    def take_care(self):
+        print(f'{self.name} is watering the plants...')
+        for plant in self.plants_to_care:
+            plant.grow_all()
+
+    def take_harvest(self):
+        for plant in self.plants_to_care:
+            if plant.is_all_ripe():
+                print("Harvest is done")
+                plant.give_harvest()
+            else:
+                print("Not ready to harvest")
+
+    def poison_pests(self, pests):
+        print(f'The {pests.pest_type}s are dead')
+        pests.are_alive = False
+
+
+
+class Pests:
+    def __init__(self, pest_type, pest_quantity, plants):
+        self.pest_type = pest_type
+        self.pest_quantity = pest_quantity
+        self.plants = plants
+        self.how_many_fed = 0
+        self.are_alive = True
+
+    def eat_plants(self):
+        for plant in self.plants:
+            if self.how_many_fed == self.pest_quantity or not self.are_alive:
+                break
+            elif plant.number_of_fruits < 1:
+                continue
+
+            i = 0
+            while i < plant.number_of_fruits:
+                if self.how_many_fed == self.pest_quantity:
+                    break
+                if plant.all_fruits[i].stage > 1:
+                    print(f'{self.pest_type} ate {plant.all_fruits[i].name}')
+                    plant.all_fruits.pop(i)
+                    plant.number_of_fruits -= 1
+                    for j in range(plant.number_of_fruits):
+                        plant.all_fruits[j].index = j
+                    self.how_many_fed += 1
+                else:
+                    i += 1
 
 
 apple_tree = AppleTree(3)
@@ -180,8 +186,47 @@ tomato_bush = TomatoBush(4)
 apple_tree.grow_all()
 tomato_bush.grow_all()
 
+for i in range(1):
+    apple_tree.grow_all()
+    tomato_bush.grow_all()
+
+gardener = Gardener('Marcus', [apple_tree, tomato_bush])
+
+gardener.take_care()
+gardener.take_harvest()
+
+print('There are:', 'no' if tomato_bush.number_of_fruits < 1 else '', ' tomato, '.join(tomato.veg_type for tomato in tomato_bush.all_fruits), 'tomato on the bush')
+print('There are:', 'no' if apple_tree.number_of_fruits < 1 else '', ' apple, '.join(apple.fruit_type for apple in apple_tree.all_fruits), 'apple on the tree')
+
+apple_tree1 = AppleTree(4)
+tomato_bush1 = TomatoBush(7)
+
+gardener.plants_to_care = [apple_tree1, tomato_bush1]
+gardener.take_care()
+apple_tree1.grow_all()
+
+print('There are: ', ' tomato, '.join(tomato.veg_type for tomato in tomato_bush1.all_fruits), 'tomato on the bush')
+print('There are: ', ' apple, '.join(apple.fruit_type for apple in apple_tree1.all_fruits), 'apple on the tree')
 
 
+bugs = Pests('bug', 3, [tomato_bush1, apple_tree1])
+bugs.eat_plants()
+
+tomato_bush1.grow_all()
+
+print('There are: ', ' tomato, '.join(tomato.veg_type for tomato in tomato_bush1.all_fruits), 'tomato on the bush')
+print('There are: ', ' apple, '.join(apple.fruit_type for apple in apple_tree1.all_fruits), 'apple on the tree')
 
 
+snails = Pests('snail', 2, [apple_tree1, tomato_bush1])
+snails.eat_plants()
+snails.eat_plants()
+snails.eat_plants()
 
+print('There are: ', ' tomato, '.join(tomato.veg_type for tomato in tomato_bush1.all_fruits), 'tomato on the bush')
+print('There are: ', ' apple, '.join(apple.fruit_type for apple in apple_tree1.all_fruits), 'apple on the tree')
+
+garden = Garden(tomato_bush1, apple_tree1, [bugs, snails])
+garden.see_the_garden()
+
+gardener.poison_pests(snails)
